2f0eae9fc6e48ff1af706dbd75b20a0fd7433f99
TioBicho <vic.cisternasl@duocuc.cl>
2026-05-12 21:39:03 +0000

    actualizacion de code base

--- Ropa de Segunda Mano (2)/Ropa de Segunda Mano/Ropa-de-Segunda-Mano/src/main/java/com/E_commerce/Ropa/de/Segunda/Mano/DTO/PerfilDTO.java
+++ Ropa de Segunda Mano (2)/Ropa de Segunda Mano/Ropa-de-Segunda-Mano/src/main/java/com/E_commerce/Ropa/de/Segunda/Mano/DTO/PerfilDTO.java
@@ -1,4 +1,16 @@
 package com.E_commerce.Ropa.de.Segunda.Mano.DTO;
+import jakarta.validation.constraints.*;
+import lombok.Data;
 
+@Data
 public class PerfilDTO {
+    @NotBlack(message= "El nombre de usuario es obligatorio")
+    private String nombre;
+
+    @Size(max = 255, message= "la biografia es muy larga")
+    private String bio;
+
+    @Pattern(regexp="ADMIN|VENDEDOR|COMPRADOR", message= "Rol no valido" )
+    private String rol;
+
 }
